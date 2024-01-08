5acc116e407d46751491985775c1c9f05a37e904
ander9991 <sgranat000@citymail.cuny.edu>
2024-01-07 20:50:32 -0500

    Updated resume

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -30,7 +30,7 @@ const Socials = () => {
                     </a>
                 </li>
                 <li className='flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500'>
-                    <a href="/resume.pdf" className='flex justify-between items-center w-full text-white' target='_blank' download={true}>
+                    <a href="/Steven_Granaturov_Resume.pdf" className='flex justify-between items-center w-full text-white' target='_blank' download={true}>
                         <> 
                         Resume <BsFillPersonLinesFill size={30} />
                         </>
