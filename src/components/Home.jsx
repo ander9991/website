ba99ab1f6839ed7c5d7cbd239286181c537ce7cd
ander9991 <sg8002@nyu.edu>
2024-01-13 17:22:01 -0500

    Update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -25,7 +25,7 @@ const Home = () => {
             </div>
 
             <div>
-                <img src={HeroImage} alt='my profile' className='rounded-2xl mx-auto w-2/3 md:w-full' />
+                <img src={HeroImage} alt='my profile' className='rounded-2xl mx-auto w-2/3 md:w-full sm:h-50' />
             </div>
         </div>
     </div>
